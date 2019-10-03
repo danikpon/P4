a6cf1747e31f446c2bb559c167cdfb19c28e2364
danik <danikpon123@gmail.com>
2019-10-03 22:01:24 +0300

    0

---
+++ 4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+		cout << "Введите значение а: ";
+		cin >> a;
+		cout << "\nВведите значение b: ";
+		cin >> b;
+
+	c = a;
+	a = b;
+	b = c;
+		cout << "\n Обмен с помощью дополнительной переменной \n" << "a = " << a << "\nb = " << b;
+
+	a = a + b;
+	b = a - b;
+	a = a - b;
+		cout << "\n Обмен без помощи дополнительной переменной \n" << "a = " << a << "\nb = " << b;
+	return 0;
+}
